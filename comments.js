7bea867d5acd6ba4bacba8c650bf61a81e4c9fc6
Lucas Tavares <145068285+lucastavares098@users.noreply.github.com>
2024-01-22 12:24:39 +0000

    Update

--- comments.js
+++ comments.js
@@ -1,1 +1,13 @@
 // Create web server
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
